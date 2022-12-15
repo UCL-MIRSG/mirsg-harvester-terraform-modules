ea68651899d84807718e7e4ea1bf90196f04a762
Daniel Matthews <d.matthews@ucl.ac.uk>
2022-12-15 15:01:32 +0000

    Use pre-existing images

--- tf/os_image.tf
+++ tf/os_image.tf
@@ -1,8 +1,13 @@
-resource "harvester_image" "os_image" {
-  name         = var.os_image.name
-  namespace    = var.os_image.namespace
-  display_name = var.os_image.display_name
-  source_type  = var.os_image.source_type
-  url          = var.os_image.url
-}
+# Example of creating a new OS image
+# resource "harvester_image" "os_image" {
+#   name         = var.os_image.name
+#   namespace    = var.os_image.namespace
+#   display_name = var.os_image.display_name
+#   source_type  = var.os_image.source_type
+#   url          = var.os_image.url
+# }
 
+data "harvester_image" "os_image" {
+  display_name = var.os_image.display_name
+  namespace    = var.os_image.namespace
+}
