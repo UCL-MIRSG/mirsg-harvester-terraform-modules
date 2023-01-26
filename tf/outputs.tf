9b3b16bb2c37f17d66134e487c4f2fdbffabf47b
Daniel Matthews <d.matthews@ucl.ac.uk>
2023-01-26 10:06:15 +0000

    Remove `tf` and `scripts` folders

--- tf/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "db_vm_ip_address" {
-    value = harvester_virtualmachine.db.network_interface[0].ip_address
-}
-
-output "web_vm_ip_address" {
-    value = harvester_virtualmachine.web.network_interface[0].ip_address
-}
