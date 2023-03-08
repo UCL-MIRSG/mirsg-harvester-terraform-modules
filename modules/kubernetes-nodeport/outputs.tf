03443a677397c455a5dd3a828e474476ac802915
Daniel Matthews <d.matthews@ucl.ac.uk>
2023-03-08 17:20:09 +0000

    Add `cluster_ip` output.

--- modules/kubernetes-nodeport/outputs.tf
+++ modules/kubernetes-nodeport/outputs.tf
@@ -2,3 +2,8 @@ output "node_port" {
   description = "The node port value assigned by Kubernetes."
   value = kubernetes_service.nodeport.spec[0].port
 }
+
+output "cluster_ip" {
+  description = "The node port value assigned by Kubernetes."
+  value = kubernetes_service.nodeport.spec[0].cluster_ip
+}
