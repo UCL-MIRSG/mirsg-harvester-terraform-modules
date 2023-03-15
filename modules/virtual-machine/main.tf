d0ba9aee8237c8cb2511d80692f00544be9e6d1c
Daniel Matthews <d.matthews@ucl.ac.uk>
2023-03-15 14:51:33 +0000

    Make cloud-init user data an input.

--- modules/virtual-machine/main.tf
+++ modules/virtual-machine/main.tf
@@ -51,11 +51,7 @@ resource "harvester_virtualmachine" "vm" {
   }
 
   cloudinit {
-    user_data = templatefile("${path.module}/user_data.yml.tftpl", {
-      USER_NAME: var.user,
-      USER_PASSWORD_HASH: var.password_hash
-      USER_PUBLIC_KEY: var.public_key
-    })
+    user_data = var.user_data
     network_data = ""
   }
 }
