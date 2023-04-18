8d3855b455b73bc93e984be5f4cc18a9273d6717
Daniel Matthews <d.matthews@ucl.ac.uk>
2023-04-18 18:19:46 +0100

    `metadata` is a list so get zero index.

--- modules/kubernetes-clusterip/outputs.tf
+++ modules/kubernetes-clusterip/outputs.tf
@@ -1,4 +1,4 @@
 output "service_name" {
   description = "The node port value assigned by Kubernetes."
-  value       = kubernetes_service.clusterip.metadata.name
+  value       = kubernetes_service.clusterip.metadata[0].name
 }
